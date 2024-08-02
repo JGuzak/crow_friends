53b410afa9025ee26284ded0e5f644e33e42b470
Jordo Workstation <jguzak@live.com>
2024-08-02 00:57:21 -0700

    it's basic but working

--- jf-poly-driver.lua
+++ jf-poly-driver.lua
@@ -1,38 +1,47 @@
 -- A script for driving JF in poly synth mode.
 
 local last_note_trigger_time = 0
+local last_gate_state = false
+local last_time_voltage = 5.0
 
-function roughly_equivalent(value, comparator, delta)
-  lower_bound = comparator - delta
-  upper_bound = comparator + delta
-
-  if lower_bound <= value and value <= upper_bound then
-    return true
+-- TODO: trigger voices with just pitch changes as well as gates. In theory, the gate handler could be removed
+function process_pitch_input(i)
+  -- If notes overlap, trigger a new voice anyway
+  if last_gate_state then
+    ii.jf.play_note(input[1].volts, 5.0)
   end
-
-  return false
 end
 
-function process_pitch(i)
-  print("note: " .. i.note)
-
-  if (time() - last_note_trigger_time) > 20 then
-    ii.jf.play_note(i.note/12, 5.0)
+function process_gate(state)
+  -- ii.jf.get('time')
+  last_gate_state = state
+  if state then
+    if (time() - last_note_trigger_time) > 5 then
+      ii.jf.play_note(input[1].volts, 5.0)
+    end
+    last_note_trigger_time = time()
   end
-  -- if gate_state then
-  -- end
-  -- last_callback_changed_pitch = true
-  last_note_trigger_time = time()
-  -- print(last_note_trigger_time)
 end
+
+-- TODO: add variable amplitude based on knob positions
+-- ii.jf.event = function(e, value)
+--   if e.name =="time" then
+--     last_time_voltage = (10.0 - value) / 2.0
+--   end
+-- end
 
 function init()
   crow.reset()
   ii.jf.mode(1)
   ii.jf.run_mode(1)
 
-  input[1].mode("scale", {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11})
-  -- input[1].hysteresis = 0.4
-  -- input[1].time = 0.15
-  input[1].scale = process_pitch
+  -- input[1].mode("stream", 0.0001)
+  -- input[1].change = function (x)
+  --   delay(process_pitch_input(x), 0.0001)
+  -- end
+
+  input[2].mode("change", 4.0, 0.15, "both")
+  input[2].change = function (x)
+    delay(process_gate(x), 0.0001)
+  end
 end
